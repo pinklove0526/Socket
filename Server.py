31ab1feea93b901d938391147ba85c954312c741
Jellyfish Ng <72180039+pinklove0526@users.noreply.github.com>
2021-06-19 20:45:34 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,18 +4,14 @@ import tkinter
 HOST = "127.0.0.1"
 PORT = 65432
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.listen(1)
-print("Waiting for client")
-conn, addr = accept()
-try:
-    print("Connected by: ", addr)
-    while True:
-        data = conn.recv(1024)
-        if len(data) > 0:
-            print("Server receive: " + data.decode("utf8"))
-    except KeyboardInterrupt:
-        conn.close()
-    finally:
-        conn.close()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by ', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+                conn.sendall(data)
