3c461ac4cdbf179d8ae3a1c925e0bbf558e086fd
Jellyfish Ng <72180039+pinklove0526@users.noreply.github.com>
2021-06-20 21:20:18 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,17 +1,34 @@
 import socket
-import tkinter
+import os
+from _thread import *
 
-HOST = "127.0.0.1"
-PORT = 65432
+ServerSocket = socket.socket()
+host = '127.0.0.1'
+port = 65432
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by ', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-                conn.sendall(data)
+print('Dang cho ket noi...')
+ServerSocket.listen(5)
+
+
+def threaded_client(connection):
+    connection.send(str.encode('Chao mung den voi server!'))
+    while True:
+        data = connection.recv(1024)
+        reply = 'Server tra loi: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(reply))
+    connection.close()
+
+while True:
+    Client, address = ServerSocket.accept()
+    print('Da ket noi den: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(threaded_client, (Client, ))
+    ThreadCount += 1
+    print('So ket noi: ' + str(ThreadCount))
+ServerSocket.close()
